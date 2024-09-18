5e324967d66c90c966f3b2e5cf8d8aad23dbf4a0
Austin Richardson <accounts@agdr.org>
2024-09-17 22:21:14 -0700

    treesitter tweaks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,11 +132,21 @@ require("nvim-tree").setup({
 
 -- [nvim-treesitter]
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "vim", "python", "bash", "ruby", "lua" },
+  ensure_installed = { "c", "vim", "python", "bash", "ruby", "lua", "markdown" },
   auto_install = true,
   highlight = {
     enable = true,
+    -- disable = { 'markdown' },
   },
+
+  -- disable treesitter on large files
+  disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+          return true
+      end
+  end,
 }
 
 -- [deoplete]
