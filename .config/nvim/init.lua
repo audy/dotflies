ddb6ce381f6a71b2df9c10e6c1c2324d8655f96f
Austin Richardson <accounts@agdr.org>
2025-09-04 08:20:49 -0700

    mini.completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -74,24 +74,30 @@ vim.diagnostic.config({
   }
 })
 
+-- custom filetypes
+vim.filetype.add({
+  extension = {
+    nf = "nextflow",
+  },
+})
+
 -- ================================
 -- PLUGINS
 -- ================================
 
 vim.pack.add({
+  { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/gbprod/nord.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-mini/mini.completion" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/petertriho/nvim-scrollbar" },
+  { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/tpope/vim-dispatch" },
   { src = "https://github.com/tpope/vim-fugitive" },
-  { src = "https://github.com/folke/which-key.nvim" },
-  { src = "https://kylechui/nvim-surround" },
-  { src = "https://github.com/stevearc/oil.nvim" },
-  { src = "https://github.com/gbprod/nord.nvim" },
-  { src = "https://github.com/petertriho/nvim-scrollbar" },
 })
-
 
 -- ================================
 -- PLUGIN CONFIGURATION
@@ -127,7 +133,7 @@ require("nvim-treesitter.configs").setup({
   highlight = { enable = true },
   auto_install = true,
   disable = function(lang, buf)
-    local max_filesize = 1024 * 1024   -- 1MiB
+    local max_filesize = 1024 * 1024 -- 1MiB
     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
     if ok and stats and stats.size > max_filesize then
       return true
@@ -145,6 +151,9 @@ require("nvim-treesitter.configs").setup({
     },
   },
 })
+
+-- Use groovy treesitter as stand-in for nextflow
+vim.treesitter.language.register("groovy", "nf")
 
 require("oil").setup({
   columns = {
@@ -169,6 +178,62 @@ require("oil").setup({
 
 vim.cmd("colorscheme nord")
 
+require('mini.completion').setup(
+  {
+    -- Delay (debounce type, in ms) between certain Neovim event and action.
+    -- This can be used to (virtually) disable certain automatic actions by
+    -- setting very high delay time (like 10^7).
+    delay = { completion = 500, info = 100, signature = 50 },
+
+    -- Configuration for action windows:
+    -- - `height` and `width` are maximum dimensions.
+    -- - `border` defines border (as in `nvim_open_win()`; default "single").
+    window = {
+      info = { height = 25, width = 80, border = "rounded" },
+      signature = { height = 25, width = 80, border = "rounded" },
+    },
+
+    -- Way of how module does LSP completion
+    lsp_completion = {
+      -- `source_func` should be one of 'completefunc' or 'omnifunc'.
+      source_func = 'omnifunc',
+
+      -- `auto_setup` should be boolean indicating if LSP completion is set up
+      -- on every `BufEnter` event.
+      auto_setup = true,
+
+      -- A function which takes LSP 'textDocument/completion' response items
+      -- (each with `client_id` field for item's server) and word to complete.
+      -- Output should be a table of the same nature as input. Common use case
+      -- is custom filter/sort. Default: `default_process_items`
+      process_items = nil,
+
+      -- A function which takes a snippet as string and inserts it at cursor.
+      -- Default: `default_snippet_insert` which tries to use 'mini.snippets'
+      -- and falls back to `vim.snippet.expand` (on Neovim>=0.10).
+      snippet_insert = nil,
+    },
+
+    -- Fallback action as function/string. Executed in Insert mode.
+    -- To use built-in completion (`:h ins-completion`), set its mapping as
+    -- string. Example: set '<C-x><C-l>' for 'whole lines' completion.
+    fallback_action = '<C-n>',
+
+    -- Module mappings. Use `''` (empty string) to disable one. Some of them
+    -- might conflict with system mappings.
+    mappings = {
+      -- Force two-step/fallback completions
+      force_twostep = '<C-Space>',
+      force_fallback = '<A-Space>',
+
+      -- Scroll info/signature window down/up. When overriding, check for
+      -- conflicts with built-in keys for popup menu (like `<C-u>`/`<C-o>`
+      -- for 'completefunc'/'omnifunc' source function; or `<C-n>`/`<C-p>`).
+      scroll_down = '<C-f>',
+      scroll_up = '<C-b>',
+    },
+  })
+
 -- ================================
 -- LSP CONFIGURATION
 -- ================================
@@ -183,6 +248,19 @@ vim.lsp.enable({
   'ruff',
   'rust_analyzer',
   'ts_ls',
+  'nextflow_ls',
+})
+
+vim.lsp.config('nextflow_ls', {
+  cmd = { 'java', '-jar', '/Users/audy/Code/nf-language-server/language-server-all.jar' },
+  filetypes = { 'nextflow' },
+  settings = {
+    nextflow = {
+      files = {
+        exclude = { '.git', '.nf-test', 'work' },
+      },
+    },
+  },
 })
 
 -- ================================
