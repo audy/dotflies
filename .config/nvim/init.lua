c3fe745ff46f4cdc7222f51a0cdc0f392e1564ec
Austin Richardson <accounts@agdr.org>
2025-09-30 14:23:36 -0700

    nord settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -184,7 +184,36 @@ require("oil").setup({
   }
 })
 
+require("nord").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  transparent = false,        -- Enable this to disable setting the background color
+  terminal_colors = true,     -- Configure the colors used when opening a `:terminal` in Neovim
+  diff = { mode = "bg" },     -- enables/disables colorful backgrounds when used in diff mode. values : [bg|fg]
+  borders = true,             -- Enable the border between verticaly split windows visible
+  errors = { mode = "bg" },   -- Display mode for errors and diagnostics
+  -- values : [bg|fg|none]
+  search = { theme = "vim" }, -- theme for highlighting search results
+  -- values : [vim|vscode]
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = {},
+    functions = {},
+    variables = {},
+
+    -- To customize lualine/bufferline
+    bufferline = {
+      current = {},
+      modified = { italic = true },
+    },
+  },
+})
+
 vim.cmd("colorscheme nord")
+
+
 
 require('mini.completion').setup(
   {
