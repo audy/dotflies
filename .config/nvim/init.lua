9f05a709a3f3863acbf805cd9388eb7bcdf1c5ba
Austin Richardson <accounts@agdr.org>
2025-09-30 14:23:30 -0700

    devicons

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -103,6 +103,7 @@ vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/tpope/vim-dispatch" },
   { src = "https://github.com/tpope/vim-fugitive" },
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
 })
 
 -- ================================
